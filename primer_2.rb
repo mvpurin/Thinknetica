4431501d47a7a66e642d7e3482e6092bdd9a359c
mvpurin <mvpurin@gmail.com>
2021-09-20 23:12:50 +0700

    hhh

--- primer_2.rb
+++
@@ -1,7 +0,0 @@
-array = Array.new
-index = 10
-while index < 105 do
-  array.push(index)
-  index += 5
-end
-puts array
--- primer_4.rb
+++
@@ -1,9 +0,0 @@
-alphabet = ('a'..'z').to_a
-letters = Hash.new
-vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-alphabet.each do |letter|
-  if vowels.include?(letter)
-    letters[letter] = alphabet.index(letter) + 1
-  end
-end
-puts letters
